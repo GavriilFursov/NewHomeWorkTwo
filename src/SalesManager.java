821485e60ae3fa0d443415fda8c9c5bc36234c59
GavriilFursov <From.skveki1@mail.ru>
2022-08-08 22:30:05 +0300

    Adding the stat method

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -15,8 +15,39 @@ public class SalesManager {
         return max;
     }
 
+    public int stat() {
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+            result = (sum - getMax() - getMin()) / sales.length;
+
+        }
+        return result;
+    }
+
+    public int getMax() {
+        int maxValue = sales[0];
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] > maxValue) {
+                maxValue = sales[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public int getMin() {
+        int minValue = sales[0];
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] < minValue) {
+                minValue = sales[i];
+            }
+        }
+        return minValue;
+    }
+
     @Override
     public String toString() {
-        return "Самая дорогая продажа: " + maxSale();
+        return "Самая дорогая продажа: " + maxSale() + ". " + "Расчет обрезанного среднего: " + stat();
     }
 }
